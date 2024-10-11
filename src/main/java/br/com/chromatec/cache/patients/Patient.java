f810d653e4de00fa15d80e212a82ede7f4720ee3
Cesar Sportore <cesar.sportore@gmail.com>
2024-10-11 20:41:10 -0300

    app-008: Professional and Patient relationship established, some mocked data added and mapper updated for a new property name (#10)

--- src/main/java/br/com/chromatec/cache/patients/Patient.java
+++ src/main/java/br/com/chromatec/cache/patients/Patient.java
@@ -2,13 +2,10 @@ package br.com.chromatec.cache.patients;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import br.com.chromatec.cache.professionals.Professional;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "patients")
@@ -29,8 +26,11 @@ public class Patient implements Serializable {
 	@Column(nullable = false, length = 11)
 	private String documentNumber;
 
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "patients", cascade = CascadeType.ALL)
+	private List<Professional> professionals;
+
 	@Column(nullable = false)
-	private LocalDateTime registrationDateTime;
+	private LocalDateTime lastUpdated;
 
 	public Long getId() {
 		return id;
@@ -56,15 +56,19 @@ public class Patient implements Serializable {
 		this.documentNumber = documentNumber;
 	}
 
-	public LocalDateTime getRegistrationDateTime() {
-		return registrationDateTime;
+	public List<Professional> getProfessionals() {
+		return professionals;
 	}
 
-	public void setRegistrationDateTime(LocalDateTime registrationDateTime) {
-		this.registrationDateTime = registrationDateTime;
+	public void setProfessionals(List<Professional> professionals) {
+		this.professionals = professionals;
 	}
 
-//	
-//	@ManyToOne(fetch = FetchType.LAZY)
-//	private Professional professional;
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,29 +1,34 @@
-INSERT INTO professionals(name, profession, registration_date_time) VALUES ('Professional1', 'NUTRITIONIST', CURRENT_TIMESTAMP);
-INSERT INTO professionals(name, profession, registration_date_time) VALUES ('Professional3', 'NUTRITIONIST', CURRENT_TIMESTAMP);
-INSERT INTO professionals(name, profession, registration_date_time) VALUES ('Professional2', 'DIETITIAN', CURRENT_TIMESTAMP);
-INSERT INTO professionals(name, profession, registration_date_time) VALUES ('Professional4', 'DIETITIAN', CURRENT_TIMESTAMP);
-INSERT INTO professionals(name, profession, registration_date_time) VALUES ('Professional5', 'NUTRITIONIST', CURRENT_TIMESTAMP);
+INSERT INTO professionals(name, profession, last_updated) VALUES ('Professional1', 'NUTRITIONIST', CURRENT_TIMESTAMP);
+INSERT INTO professionals(name, profession, last_updated) VALUES ('Professional3', 'NUTRITIONIST', CURRENT_TIMESTAMP);
+INSERT INTO professionals(name, profession, last_updated) VALUES ('Professional2', 'DIETITIAN', CURRENT_TIMESTAMP);
+INSERT INTO professionals(name, profession, last_updated) VALUES ('Professional4', 'DIETITIAN', CURRENT_TIMESTAMP);
+INSERT INTO professionals(name, profession, last_updated) VALUES ('Professional5', 'NUTRITIONIST', CURRENT_TIMESTAMP);
 
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient1', '12345678901', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient2', '12345678902', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient3', '12345678903', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient4', '12345678904', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient5', '12345678905', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient6', '12345678906', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient7', '12345678907', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient8', '12345678908', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient9', '12345678909', CURRENT_TIMESTAMP);
-INSERT INTO patients(name, document_number, registration_date_time) VALUES ('Patient10', '12345678910', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient1', '12345678901', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient2', '12345678902', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient3', '12345678903', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient4', '12345678904', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient5', '12345678905', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient6', '12345678906', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient7', '12345678907', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient8', '12345678908', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient9', '12345678909', CURRENT_TIMESTAMP);
+INSERT INTO patients(name, document_number, last_updated) VALUES ('Patient10', '12345678910', CURRENT_TIMESTAMP);
+
+INSERT INTO professionals_patients(professional_id, patient_id) VALUES (1,1);
+INSERT INTO professionals_patients(professional_id, patient_id) VALUES (1,2);
+INSERT INTO professionals_patients(professional_id, patient_id) VALUES (2,3);
+INSERT INTO professionals_patients(professional_id, patient_id) VALUES (3,4);
 
 /*
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient1', 1, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient2', 2, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient3', 3, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient4', 4, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient5', 5, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient6', 1, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient7', 1, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient8', 2, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient9', 3, CURRENT_TIMESTAMP);
-INSERT INTO patients(name, professional_id, registration_date_time) VALUES ('Patient10', 3, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient1', 1, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient2', 2, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient3', 3, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient4', 4, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient5', 5, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient6', 1, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient7', 1, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient8', 2, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient9', 3, CURRENT_TIMESTAMP);
+INSERT INTO patients(name, professional_id, last_updated) VALUES ('Patient10', 3, CURRENT_TIMESTAMP);
 */
